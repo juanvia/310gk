9ee823053fde4a67984c2c21545427f6e4a5bb8b
Juan V. Vía <juan.via@gmail.com>
2019-06-09 14:20:44 -0300

    Changed name generatorRow for powers

--- src/anotherstuff.js
+++ src/anotherstuff.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { isEmpty, addIndex, chain,  remove, map, 
+import { isEmpty, addIndex, chain,  remove, map,
   append, repeat, uniq, concat, any, range, sum,
   findIndex, findLastIndex, sort } from "ramda";
 
@@ -11,24 +11,25 @@ import { isEmpty, addIndex, chain,  remove, map,
 // Returns all permutations  a vector -----------------------------------------------
 
 export const permutations = (tokens, subperms = [[]]) =>
-  isEmpty(tokens) ? subperms 
+  isEmpty(tokens) ? subperms
                   : addIndex(chain)((token, idx) => permutations( remove(idx, 1, tokens), map(append(token), subperms)), tokens)
 
 
 
 
-                  
-// ----- relax ------------------------------------------------------------
-// Makes more "plain" a vector (distribute its values) 
-export const relax = generatorRow => {
 
-  let toDecrementIndex = findLastIndex(e => e > 1, generatorRow)
-  let toIncrementIndex = findIndex(e => e < generatorRow[toDecrementIndex] - 1, generatorRow)
-  
-  generatorRow[toDecrementIndex] = generatorRow[toDecrementIndex] - 1
-  generatorRow[toIncrementIndex] = generatorRow[toIncrementIndex] + 1
-  
-  return generatorRow
+// ----- relax ------------------------------------------------------------
+// Makes more "plain" a vector (distribute its values)
+
+export const relax = powers => {
+
+  let toDecrementIndex = findLastIndex(e => e > 1, powers)
+  let toIncrementIndex = findIndex(e => e < powers[toDecrementIndex] - 1, powers)
+
+  powers[toDecrementIndex] = powers[toDecrementIndex] - 1
+  powers[toIncrementIndex] = powers[toIncrementIndex] + 1
+
+  return powers
 
 }
 
@@ -38,7 +39,7 @@ export const relax = generatorRow => {
 // Returns the value needed for sort a generator row
 
 const max = array => array.reduce((previous, current) => Math.max(previous, current), Number.MIN_VALUE)
-export const relevance = row => sum(row) * 1000000 + max(row) * 1000 + row.reduce((previous, current, index, array) => previous + 2 ** (array.length - index) * current, 0) 
+export const relevance = row => sum(row) * 1000000 + max(row) * 1000 + row.reduce((previous, current, index, array) => previous + 2 ** (array.length - index) * current, 0)
 
 
 
@@ -48,21 +49,21 @@ export const relevance = row => sum(row) * 1000000 + max(row) * 1000 + row.reduc
 export const makeStackedMatrixOfGenerators = (dimensions, degree) => {
 
   let stackedMatrix = []
-  
-  let generatorRow = repeat(0, dimensions)  // This generator row is for permutate and stack those permutations in the matrix.
-  generatorRow[0] = degree                  // At first all to the first one. High potential energy. High inequality. Lowest enthropy.
-  
-  
-  while(any(e => e > 1)(generatorRow)) {
-    
-    stackedMatrix = concat(stackedMatrix, uniq(permutations(generatorRow)))
-  
-    generatorRow = relax(generatorRow)      // step by step to social justice among the elements
-    
-  } 
-  
+
+  let powers = repeat(0, dimensions)  // This generator row is for permutate and stack those permutations in the matrix.
+  powers[0] = degree                  // At first all to the first one. High potential energy. High inequality. Lowest entropy.
+
+
+  while(any(e => e > 1)(powers)) {
+
+    stackedMatrix = concat(stackedMatrix, uniq(permutations(powers)))
+
+    powers = relax(powers)      // step by step to social justice among the elements
+
+  }
+
   // Don't forget the last term
-  stackedMatrix = concat(stackedMatrix, uniq(permutations(generatorRow)))
+  stackedMatrix = concat(stackedMatrix, uniq(permutations(powers)))
 
   // The turn has come for lesser degrees
   if (degree > 0) {
@@ -77,18 +78,18 @@ export const makeStackedMatrixOfGenerators = (dimensions, degree) => {
 
 
 
+// ----- makePolynomial ---------------------------------------------------
+// Returns a string with a representation of the polinomial defined by the stacked matrix
+
 const upperCases = range(65, 65+26).map(charCode => String.fromCharCode(charCode))
 const variableNames = "xyztuvw".split('')
-
-// ----- makePolynomial ---------------------------------------------------
-// Returns a string with a representation of the polinomial defined by the stacked matrix 
 
 export const makePolynomial = (coefficientsVariant, variablesVariant, stackedMatrix) => {
 
     const makeTerm = (powers, index, array) => {
-        return powers.reduce((s,p,i) =>  <span key={index}>{s}{(p === 0 ? '' : variableNames[i % variableNames.length])}<sup>{p<2?'':p}</sup></span>, <>{upperCases[index % array.length]}</>)  
+        return powers.reduce((s,p,i) =>  <span key={index}>{s}{(p === 0 ? '' : variableNames[i % variableNames.length])}<sup>{p<2?'':p}</sup></span>, <>{upperCases[index % array.length]}</>)
     }
-    let terms =  stackedMatrix.map((powers, index, array) => <span key={index}>{makeTerm(powers, index, array)}{(index < array.length-1 ? ' + ' : '')}</span>) 
+    let terms =  stackedMatrix.map((powers, index, array) => <span key={index}>{makeTerm(powers, index, array)}{(index < array.length-1 ? ' + ' : '')}</span>)
 
     return <div>
         {terms}
