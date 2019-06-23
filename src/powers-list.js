8d19642c903aa550c9eff58a3cf72e4e43b7448e
Juan V. Vía <juan.via@gmail.com>
2019-06-23 06:08:05 -0300

    Large comment in takeValidPoints

--- src/powers-list.js
+++ src/powers-list.js
@@ -1,6 +1,6 @@
 // ----- relevance --------------------------------------------------------
 // Returns the value needed for sort a generator row
-import { repeat,concat,sort, sum, takeLast, reduce,add, map, pipe, split } from 'ramda'
+import { repeat,concat,sort, sum, takeLast, reduce,add, map, pipe, split, join } from 'ramda'
 
 
 // ----- permutations -----------------------------------------------------
@@ -12,38 +12,64 @@ export const relevance = row => {
   + row.reduce((previous, current, index, array) => previous + 2 ** (array.length - index) * current, 0)
 }
 
-// ------ makeStackedMatrixOfGenerators -----------------------------------
-// Returns a matrix where each row is a generator for one term in the polynomial
-// a 'generator' is a vector containing the powers at which the correspondent 
-// independent variable must be raised
+
 
 
 
 // Delivers an array of given size of digits of given number expressed in given base
+
 const transform = (size, base, number) => pipe(
-    concat('0000000000'),         // ensure enough length for next step
-    takeLast(size),               // the length must equals the value of the "size" variable
-    split(''),                    // transform the number string representation to an array of chars
-    map(Number)                   // array of chars to array of numbers (one digit each)
-) (number.toString(base))         // send to pipe the number in <base>ary form
+
+    concat(join('',repeat('0',size))),  // ensure enough length for next step
+    takeLast(size),                     // the length must equals the value of the "size" variable
+    split(''),                          // transforms the number string representation to an array of chars
+    map(Number)                         // array of chars to array of numbers (one digit each)
+
+) (number.toString(base))               // send to pipe the number in <base>ary form
 
 
-const takeValidsForDegree = (dimensions, degree) => {
+
+// Let A be a n-dimensional space of integers in the range (0...m)
+// Where n < 6 and m < 10 (i.e. the elements of V are digits)
+// The function below gives us a subset of A whose points p fullfill the contstraint:
+// The sum of digits of p is less or equal to m
+// Example:
+//
+// 2      (2,0)  (2,1)  (2,2)
+// 1      (1,0)  (1,1)  (1,2)
+// 0      (0,0)  (0,1)  (0,2)
+//
+//          0      1      2
+//  
+// In this example n=2 (two-dimensional space) and also m=2 (max value in table) 
+// then the valid points are (1,0), (0,0), (0,1), (2,0), (1,1) and (0,2)
+// because the digit sum of all of those is less or equal to 2
+// Note that in base-3 (m+1) the first nine numbers are 00, 01, 02, 10, 11, 12, 20, 21 and 22 
+// -------------------------------------------------------------------------------
+// There is a bijection betwen A and the non-negative integers from
+// zero to (m+1)^n. Also A can be viewed as the set of those integers expressed in
+// base (m+1). The algorithm takes advantage of that.
+// Reminder:
+// - n denotes the numbers of dimensions of A (the phase space)
+// - m is the max value the coordinates that the points of A can have.
+// - also m is the max value the sum of coordinates of the points p can have.
+
+const takeValidPoints = (n, m) => {
   
   // Initialize the list of valid points to empty
   let valids = []
   
   // All the posibilities are taken into account
-  let phaseSpaceCardinal = (degree + 1) ** dimensions
+  let phaseSpaceCardinality = (m + 1) ** n
 
-  // Visit the entire phase space searching for good points
-  for (let ordinal = 0; ordinal < phaseSpaceCardinal; ++ordinal) {
+  // Visits the entire phase space searching for good points
+  for (let ordinal = 0; ordinal < phaseSpaceCardinality; ++ordinal) {
 
     // the number expressed in base <degree+1>. This is the punch line!
-    let point = transform(dimensions, degree+1, ordinal)
+    let point = transform(n, m+1, ordinal)
 
-    // the point tested (and adopted if its sum is correct)
-    if (reduce(add, 0)(point) <= degree) {
+    // the point tested (and adopted if its digits sum is adequate)
+    if (reduce(add, 0)(point) <= m) {
 
       valids.push(point)
 
@@ -57,6 +83,9 @@ const takeValidsForDegree = (dimensions, degree) => {
 
 
 
+// Returns a matrix where each row is a powers array for one term in the polynomial
+// a 'powers array' is a vector containing the powers at which the correspondent 
+// independent variable must be raised
 
 export const makeStackedMatrixOfGenerators = (dimensions, degree) => {
   
@@ -66,7 +95,7 @@ export const makeStackedMatrixOfGenerators = (dimensions, degree) => {
   }
 
   // Gathering the points whose sum of elements is less or equal to the given degree
-  let stack = takeValidsForDegree(dimensions, degree)
+  let stack = takeValidPoints(dimensions, degree)
 
   // On delivery be polite and give a neat, ordered list
   const byRelevance = (a, b) => relevance(b) - relevance(a)
